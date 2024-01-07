603f936e967e777bba8aae2cc0cc7c4fe30a12b8
ASKK <102436728+ASKiere@users.noreply.github.com>
2024-01-07 13:04:11 -0500

    lua

---
+++ lua/UIBloxInGameConfig.lua
@@ -0,0 +1,25 @@
+local CorePackages = game:GetService("CorePackages")
+local CoreGui = game:GetService("CoreGui")
+
+local SharedFlags = require(CorePackages.Workspace.Packages.SharedFlags)
+local UIBloxFlags = SharedFlags.UIBlox
+
+local RobloxGui = CoreGui:WaitForChild("RobloxGui")
+
+return {
+	fixLoadableImageLoadingFailed = UIBloxFlags.GetFFlagFixLoadableImageLoadingFailed(),
+	useNewGenericTextLabelProps = UIBloxFlags.GetFFlagUIBloxUseNewGenericTextLabelProps(),
+	enableRightSideGadgetView = UIBloxFlags.GetFFlagUIBloxUseRightSideGadget(),
+	useNewThemeColorPalettes = SharedFlags.GetFFlagLuaAppUseUIBloxColorPalettes()
+		and UIBloxFlags.GetUIBloxUseNewThemeColorPalettes(),
+	pillGroupAutomaticSize = UIBloxFlags.GetFFlagUIBloxPillGroupAutomaticSize(),
+	vrAlignPanel3DUnderInGamePanel = UIBloxFlags.GetFFlagUIBloxVRAlignPanel3DUnderInGamePanel(),
+	vrFixUIJitter = UIBloxFlags.GetFFlagUIBloxVRFixUIJitter(),
+	dualVRLaserPointers = UIBloxFlags.GetFFlagUIBloxVRDualLaserPointers(),
+	useNewPlayerCount = UIBloxFlags.GetFFlagUIBloxUseNewPlayerCount(),
+	enableToastButton = UIBloxFlags.GetFFlagUIBloxEnableToastButton(),
+	usePlatformContentKeyLabels = UIBloxFlags.GetFFlagUIBloxUsePlatformContentKeyLabels(),
+	enableNewMenuLayout = UIBloxFlags.GetFFlagUIBloxEnableNewMenuLayout(),
+	enableRoDSDesignTokenSupport = UIBloxFlags.GetFFlagEnableRoDSDesignTokenSupportInCoreScripts(),
+	useTokensInPlayerTile = UIBloxFlags.GetFFlagUIBloxUseTokensInPlayerTile(),
+}
